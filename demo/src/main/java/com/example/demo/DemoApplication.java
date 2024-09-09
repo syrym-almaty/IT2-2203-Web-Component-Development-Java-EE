f62a70a6bdc2ca0e4919f85911511fca8a98a089
Ad1L2005 <adil.nurgozha@gmail.com>
2024-09-09 09:32:57 +0500

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// this is comments from Adil
+	// this is comments from Adil test
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
