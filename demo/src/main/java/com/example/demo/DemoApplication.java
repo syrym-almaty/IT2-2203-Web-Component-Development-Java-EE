768b23e7abbb591657c0babff9d740a522e0e32e
gulnaz25 <gulllekeauganova@gmail.com>
2024-09-09 11:07:24 +0600

    adding end point hello world

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//Hello world
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
