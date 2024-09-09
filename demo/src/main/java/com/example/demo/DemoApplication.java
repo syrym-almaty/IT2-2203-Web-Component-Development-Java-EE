3fde2e1a306db4403f6c141033b484223b5920f8
Dias-dot-art <129710179+Dias-dot-art@users.noreply.github.com>
2024-09-09 11:14:08 +0500

    adding the comments

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+//	this is my comments from Dias
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
