c19f9fc09d9173a571b221c4649ab313437db204
Ad1L2005 <adil.nurgozha@gmail.com>
2024-09-09 09:27:02 +0500

    i am adil i have added the comments to the demo java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// this is comments from Adil
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
