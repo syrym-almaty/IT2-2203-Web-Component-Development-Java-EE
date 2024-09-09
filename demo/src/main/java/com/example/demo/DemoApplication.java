b923b77798136cd151ae3e965851f6dfcfb85c64
Bostanbek Maratali <makebostanbek@MacBook-Air-Bostanbek.local>
2024-09-09 10:40:09 +0500

    adding new comments

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
+//	this is my comment and my name is Muratali
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
