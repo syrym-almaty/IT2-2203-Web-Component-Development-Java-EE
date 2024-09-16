d9ab45875c4f1283261f3b584c4fee1b4148a0ee
Rassul <rasa.laik@gmail.com>
2024-09-16 09:23:10 +0500

    adding Controller, Main page

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hello Rasul");
+		System.out.println("Hello Rasul madi chort");
 	}
 
 }
