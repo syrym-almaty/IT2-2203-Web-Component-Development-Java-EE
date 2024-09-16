7963ffa147fec1a8000c59890bdc2d5f88802bb3
Dias-dot-art <129710179+Dias-dot-art@users.noreply.github.com>
2024-09-16 10:46:47 +0500

    Added Hello World endpoint

---
+++ demo/src/main/java/com/example/demo/DiasController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiasController {
+    @GetMapping("/hello")
+    public String helloWorld() {return "Hello World!"; }
+}
