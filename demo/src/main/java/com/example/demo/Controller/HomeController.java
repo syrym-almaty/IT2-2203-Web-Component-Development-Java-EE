8af1460092bf07d70805e010f31ccddca8922650
Rassul <rasa.laik@gmail.com>
2024-09-16 09:07:56 +0500

    adding Controller, Main page

---
+++ demo/src/main/java/com/example/demo/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+    @GetMapping("/home")
+    public String PageHome(){
+        return "Hello world";
+    }
+}
