371630b53febf218c83244755f84055927a3bccb
gulnaz25 <gulllekeauganova@gmail.com>
2024-09-16 11:25:02 +0600

    test

--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 @SpringBootApplication
-public class DemoApplication {
-//Hello world
+public class Main {
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
