a3ee49b861a9576f6b67d587674a45f234e2265b
Rassul <rasa.laik@gmail.com>
2024-10-06 20:00:43 +0500

    deleting merge conflict comments

--- demo/src/main/java/com/example/demo/entity/Student.java
+++ demo/src/main/java/com/example/demo/entity/Student.java
@@ -3,10 +3,8 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-<<<<<<< HEAD
 import jakarta.persistence.Column;
-=======
->>>>>>> origin/student-rasul
+
 import org.hibernate.annotations.GenericGenerator;
 import java.util.UUID;
 
@@ -16,20 +14,19 @@ public class Student {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
-        name = "UUID",
-        strategy = "org.hibernate.id.UUIDGenerator"
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
     )
-<<<<<<< HEAD
+
     @Column(columnDefinition = "uuid")
-=======
->>>>>>> origin/student-rasul
     private UUID id;
 
     private String name;
     private String email;
 
     // Constructors
-    public Student() {}
+    public Student() {
+    }
 
     public Student(String name, String email) {
         this.name = name;
@@ -53,15 +50,11 @@ public class Student {
         this.name = name;
     }
 
-<<<<<<< HEAD
-	public String getEmail() {
-=======
-    public String getEmail() {
->>>>>>> origin/student-rasul
-        return email;
+    public String getEmail () {
+            return email;
+        }
+    public void setEmail (String email){
+            this.email = email;
+        }
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,14 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-<<<<<<< HEAD
 import java.util.UUID;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, UUID> {
-=======
-
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
->>>>>>> origin/student-rasul
+
 }
