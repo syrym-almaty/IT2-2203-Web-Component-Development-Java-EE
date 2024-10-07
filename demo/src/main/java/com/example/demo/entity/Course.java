25d32f137895f5f5dfd8fb5d3084f177a2ea6cc4
Rassul <rasa.laik@gmail.com>
2024-10-07 12:45:20 +0500

    Created Course Entity and joined ManyToMany to Students using lombok

---
+++ demo/src/main/java/com/example/demo/entity/Course.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Course name is required")
+    private String name;
+
+    @NotBlank(message = "Course code is required")
+    @Column(unique = true)
+    private String code;
+
+    @ManyToMany(mappedBy = "courses")
+    private Set<Student> students = new HashSet<>();
+}
--- demo/src/main/java/com/example/demo/entity/Student.java
+++ demo/src/main/java/com/example/demo/entity/Student.java
@@ -1,11 +1,11 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
 import org.hibernate.annotations.GenericGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -56,5 +56,17 @@ public class Student {
     public void setEmail (String email){
             this.email = email;
         }
-    }
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "enrollments",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private Set<Course> courses = new HashSet<>();
+
+    // GPA field
+    private Double gpa;
+}
 
