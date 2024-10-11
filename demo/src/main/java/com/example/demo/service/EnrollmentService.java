0250d8fab61b5a61a255a4fb30844a3900ebd195
Rassul <rasa.laik@gmail.com>
2024-10-11 10:08:11 +0500

    addinng service EnrollmentService

---
+++ demo/src/main/java/com/example/demo/service/EnrollmentService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.*;
+import com.example.demo.exception.*;
+import com.example.demo.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+
+@Service
+public class EnrollmentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public void enrollStudentInCourse(UUID studentId, Long courseId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found"));
+
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found"));
+
+        if (student.getCourses().size() >= 5) {
+            throw new BusinessException("Student cannot enroll in more than 5 courses");
+        }
+
+        if (course.getStudents().size() >= 30) {
+            throw new BusinessException("Course cannot have more than 30 students");
+        }
+
+        student.getCourses().add(course);
+        course.getStudents().add(student);
+
+        studentRepository.save(student);
+        courseRepository.save(course);
+    }
+}
