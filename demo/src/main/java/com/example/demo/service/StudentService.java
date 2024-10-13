b0561b7537ac26d6ab74125cb6bb7c70da723578
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-10-13 20:48:59 +0600

    Imports

--- demo/src/main/java/com/example/demo/service/StudentService.java
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.UUID;
+
 public class StudentService {
 }
