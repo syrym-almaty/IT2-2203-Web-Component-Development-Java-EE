1d43a8b309fd51bf207cb72445b66f6f6ca89a84
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-10-13 20:44:00 +0600

    imports and interface

--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public class StudentRepository {
+import com.example.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
