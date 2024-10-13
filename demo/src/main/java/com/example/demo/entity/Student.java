6ca707e543f3179df26137e2a731ed0dca53a37f
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-10-13 20:39:42 +0600

    Imports for Student java

--- demo/src/main/java/com/example/demo/entity/Student.java
+++ demo/src/main/java/com/example/demo/entity/Student.java
@@ -1,4 +1,10 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
+
 public class Student {
 }
