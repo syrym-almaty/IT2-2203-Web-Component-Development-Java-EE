8a711ab556d90721df4bcc762ca771032be5609b
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-10-13 22:51:38 +0600

    Imports

--- demo/src/main/java/com/example/demo/entity/Course.java
+++ demo/src/main/java/com/example/demo/entity/Course.java
@@ -1,4 +1,16 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class Course {
 }
