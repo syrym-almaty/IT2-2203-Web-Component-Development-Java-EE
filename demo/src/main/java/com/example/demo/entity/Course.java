0175b8870fedc1880bce002842fc9478df8226e6
Adikrichy <adil.erzhanoc.70@gmail.com>
2024-10-13 23:07:18 +0600

    qq21

--- demo/src/main/java/com/example/demo/entity/Course.java
+++ demo/src/main/java/com/example/demo/entity/Course.java
@@ -1,5 +1,4 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -11,11 +10,31 @@ import org.hibernate.annotations.GenericGenerator;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Course name is required")
+    private String name;
+
+    @NotBlank(message = "Course code is required")
+    @Column(unique = true)
+    private String code;
+
+    @Getter
+    @Setter
+    @NotBlank(message = "Course credit is required")
+    @Min(value = 2, message = "Course credits must be at least 2")
+    @Max(value = 6, message = "Course credits must not exceed 6")
+    private int credits;
 
 
-
-
-
-
-
+    @ManyToMany(mappedBy = "courses")
+    private Set<Student> students = new HashSet<>();
 }
